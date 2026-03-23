4d0df4836ce0be876259fc7833d00549746cd529
Sayed Abdul Rajjak <abdulrazzak8805@gmail.com>
2026-03-24 03:08:48 +0530

    temporary admin creator

--- backend/core/views.py
+++ backend/core/views.py
@@ -137,3 +137,11 @@ def resume_download(request):
         as_attachment=True,
         filename=filename
     )
+
+from django.contrib.auth.models import User
+
+def create_admin(request):
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@gmail.com', 'admin123')
+        return JsonResponse({"message": "Admin created"})
+    return JsonResponse({"message": "Admin already exists"})
